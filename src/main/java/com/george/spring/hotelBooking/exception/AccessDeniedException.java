2889696156102aab4e6c3ead815a8e40e1a98351
George <george.groshev1997@gmail.com>
2023-04-25 15:03:03 +0300

    01.6

---
+++ src/main/java/com/george/spring/hotelBooking/exception/AccessDeniedException.java
@@ -0,0 +1,5 @@
+package com.george.spring.hotelBooking.exception;
+
+public class AccessDeniedException extends RuntimeException {
+    public AccessDeniedException() { }
+}
--- src/main/java/com/george/spring/hotelBooking/web/security/JwtTokenProvider.java
+++ src/main/java/com/george/spring/hotelBooking/web/security/JwtTokenProvider.java
@@ -1,14 +1,24 @@
 package com.george.spring.hotelBooking.web.security;
 
+import com.george.spring.hotelBooking.domain.user.User;
+import com.george.spring.hotelBooking.exception.AccessDeniedException;
 import com.george.spring.hotelBooking.service.UserService;
 import com.george.spring.hotelBooking.service.props.JwtProperties;
+import com.george.spring.hotelBooking.web.dto.auth.JwtResponse;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
 
 @Service
 @RequiredArgsConstructor
@@ -22,4 +32,75 @@ public class JwtTokenProvider {
         this.key = Keys.hmacShaKeyFor(jwtProperties.getSecret().getBytes());
     }
 
+    public String createAccessToken(Long userId,
+                                    String username) {
+        Claims claims = Jwts.claims().setSubject(username);
+        claims.put("id", userId);
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + jwtProperties.getAccess());
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(key)
+                .compact();
+    }
+    public String createRefreshToken(Long userId, String username) {
+        Claims claims = Jwts.claims().setSubject(username);
+        claims.put("id", userId);
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + jwtProperties.getRefresh());
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(key)
+                .compact();
+    }
+    public JwtResponse refreshUserTokens(String refreshToken) {
+        JwtResponse jwtResponse = new JwtResponse();
+        if (!validateToken(refreshToken)) {
+            throw new AccessDeniedException();
+        }
+        Long userId = Long.valueOf(getId(refreshToken));
+        User user = userService.getById(userId);
+        jwtResponse.setId(userId);
+        jwtResponse.setUsername(user.getUsername());
+        jwtResponse.setAccessToken(createAccessToken(userId, user.getUsername()));
+        jwtResponse.setRefreshToken(createRefreshToken(userId, user.getUsername()));
+        return jwtResponse;
+    }
+    public Authentication getAuthentication(String token) {
+        String username = getUsername(token);
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    private boolean validateToken(String token) {
+        Jws<Claims> claims = Jwts
+                .parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token);
+        return !claims.getBody().getExpiration().before(new Date());
+    }
+    private String getId(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .get("id")
+                .toString();
+    }
+    private String getUsername(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
 }
